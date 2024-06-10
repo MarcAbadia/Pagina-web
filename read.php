8e09d799e84d6bd3a71af40d7fa41a172d432010
Tu Nombre <you@example.com>
2024-06-10 09:50:42 +0200

    Leer

--- read.php
+++ read.php
@@ -4,11 +4,28 @@
     $sql = "SELECT id, nombre, telefono, correo, mensaje FROM contactos";
     $result = $conn->query($sql);
 
-    echo "<pre>";
-    var_dump($result);
-    echo "<pre>";
-?>
 
+?>
+<h1>Leer mensajes</h1>
+
+<div class="leer">
+<?php
+    while($row = $result->fetch_assoc()){
+        ?>
+        <div class="info">
+                <div class="id"><?=$row['id']?></div>
+                <div class="nombre"><?=$row['nombre']?></div>
+                <div class="telefono"><?=$row['telefono']?></div>
+                <div class="correo"><?=$row['correo']?></div>
+                <div class="mensaje"><?=$row['mensaje']?></div>
+        </div>
+
+
+        <?php
+    }
+
+?>
+</div>
 
 
 <?php
